59bec938ab878145fea97f35d2f983ae6c85a459
Nicholas Johnson <nicholas.c.j@gmail.com>
2016-05-12 09:04:44 -0700

    undo not smart branching.

--- src/pg_kafka.c
+++ src/pg_kafka.c
@@ -130,6 +130,8 @@ static void pg_xact_callback(XactEvent event, void *arg) {
     break;
   case XACT_EVENT_COMMIT:
   case XACT_EVENT_PREPARE:
+  case XACT_EVENT_PRE_COMMIT:		
+  case XACT_EVENT_PRE_PREPARE:
   default:
     break;
   }
